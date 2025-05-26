1241d4b0542e67e77b582d6927511b30b751d3fb
oculusK <kay5990@naver.com>
2025-05-26 19:43:56 +0900

    feature Rabbit 기능 구현

--- src/main/java/com/nhnacademy/notifyservice/NotifyServiceApplication.java
+++ src/main/java/com/nhnacademy/notifyservice/NotifyServiceApplication.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.notifyservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
+@EnableRabbit
 public class NotifyServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/nhnacademy/notifyservice/config/RabbitConfig.java
+++ src/main/java/com/nhnacademy/notifyservice/config/RabbitConfig.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +39,11 @@ public class RabbitConfig {
      * Dead Letter Exchange 이름 상수입니다.
      */
     private static final String EMAIL_DLX = "email-exchange.dlx";
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
     /**
      * 이메일 발송용 원본 큐를 생성합니다.
